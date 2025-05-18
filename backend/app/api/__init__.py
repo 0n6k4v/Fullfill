658fa6295d2b2e9dcde7ea57e21baf60c629740f
0n6k4v-Coder <kawee26code@gmail.com>
2025-05-18 22:01:51 +0700

    fix api path

--- backend/app/api/__init__.py
+++ backend/app/api/__init__.py
@@ -2,7 +2,6 @@
 API module for the Fullfill application.
 """
 
-# ลดการ import ลงเพื่อป้องกัน circular imports
 from app.api.router import api_router
 
 __all__ = ["api_router"]
--- backend/app/api/router.py
+++ backend/app/api/router.py
@@ -20,4 +20,4 @@ api_router.include_router(image.router, prefix="/images", tags=["Images"])
 api_router.include_router(provinces.router, prefix="/provinces", tags=["Provinces"])
 api_router.include_router(districts.router, prefix="/districts", tags=["Districts"])
 api_router.include_router(subdistricts.router, prefix="/subdistricts", tags=["Subdistricts"])
-api_router.include_router(dashboard.router, prefix="/dashboard", tags=["Dashboard"]) # Add this line
+api_router.include_router(dashboard.router, prefix="/dashboard", tags=["Dashboard"])
